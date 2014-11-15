363b447541794ba57271c3a201c80b7ac7855f39
Lionel Rudaz <lionelrudaz@icloud.com>
2014-11-15 14:15:09 +0100

    Finish tutorial

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :contact do
-    first_name "MyString"
-last_name "MyString"
-email "MyString"
-title "MyString"
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.email }
+    title { Faker::Name.title }
   end
 
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require "spec_helper"
 
-RSpec.describe Contact, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Contact do
+#   it "should have a factory" do
+#     expect(FactoryGirl.build(:contact)).to be_valid
+#   end
+
+#   context "validations" do
+#     it { should validate_uniqueness_of(:email) }
+#     it { should validate_presence_of(:first_name) }
+#     it { should validate_presence_of(:last_name) }
+#     it { should validate_presence_of(:email) }
+#     it { should validate_presence_of(:title) }
+#   end
 end
