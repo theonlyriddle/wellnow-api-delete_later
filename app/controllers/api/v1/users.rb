ff0fe21d93532c5b42a1e743bbfab810d25f3cd1
Lionel Rudaz <lionelrudaz@icloud.com>
2015-02-18 00:02:58 +0100

    Save new users

--- app/controllers/api/v1/users.rb
+++ app/controllers/api/v1/users.rb
@@ -17,6 +17,51 @@ module API
         get ":id", root: "user" do
           User.where(id: permitted_params[:id]).first!
         end
+
+        desc "Create an account"
+        params do
+          group :user, type: Hash do
+            requires :email, :type => String, :desc => "User email"
+            requires :password, :type => String, :desc => "User password"
+            optional :firstname, type: String
+            optional :lastname, type: String
+          end
+        end
+        post '' do
+          email = params[:user][:email]
+          password = params[:user][:password]
+
+           if email.nil? or password.nil?
+             error!({:error_code => 404, :error_message => "Invalid email or password."}, 401)
+             return
+           end
+
+           user = User.find_by(email: email.downcase)
+           if !user.nil?
+              error!({:error_code => 404, :error_message => "Existing user!"}, 401)
+              return
+           else
+             #user.ensure_authentication_token
+             #user.save
+              # status(201){
+              #  status: 'ok', 
+              #  token: user.authentication_token 
+              # }
+              # render json: { :user_token => user.authentication_token, :user_email => user.email, :token_type => "bearer" }, status: 201
+              # {
+              #   :user_token => user.authentication_token, :user_email => user.email, :user_id => user.id, :token_type => "bearer"
+              # }
+
+              u = User.create!(
+                :email => email,
+                :password => password,
+                :firstname => params[:user][:firstname],
+                :lastname => params[:user][:lastname]
+              )
+
+              {user: {email: email, password: password, id: u.id}}
+           end
+        end
       end
     end
   end
