cb10ef3fad913ec5d7d91cc5b36219c4b5c2820e
Lionel Rudaz <lionelrudaz@icloud.com>
2015-06-16 22:41:55 +0200

    Remove cached on category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,6 +1,6 @@
 class CategorySerializer < ActiveModel::Serializer
-    cached
-    delegate :cache_key, to: :object
+    # cached
+    # delegate :cache_key, to: :object
 
     attributes :id, :title, :description, :rank, :created_at, :updated_at
 end
